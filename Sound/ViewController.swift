3e2e2ccf5d0b848efc9765dd5e183c7582515d66
舟木正憲 <funakimfunaki@gmail.com>
2018-12-29 12:59:43 +0900

    サウンド機能の実装

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+// サウンドや動画の再生機能が含まれる
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonTapped(sender: UIButton) {
+        let url = URL(fileURLWithPath: Bundle.main.bundlePath).appendingPathComponent("sample.mp3")
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.delegate = self
+            player.play()
+        } catch {
+            print("Failed")
+        }
+    }
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Finished!")
+    }
 
 }
 
